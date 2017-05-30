05d71e7049d976f84aebbad4da80f3463e335296
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2017-05-30 20:52:37 +0200

    Added IPID generator.

---
+++ utils.js
@@ -0,0 +1,40 @@
+var IPID = (function() {
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    var num = chars.length;
+    var len = 3;
+    var end = len - 1;
+    var arr = [];
+    var pointer = end;
+    var i;
+
+    for (i = 0; i < len; i++)
+        arr[i] = 0;
+
+    return function() {
+        var shifted = false;
+        var last = arr[end];
+
+        var id = '';
+        for (i = 0; i < len; i++)
+            id += chars[arr[i]];
+
+        if (last == (num - 1)) {
+            while (arr[pointer] == (num - 1)) {
+                arr[pointer] = 0;
+                pointer--;
+            }
+
+            if (pointer != -1)
+                arr[pointer] = (arr[pointer] + 1) % num
+
+            pointer = end;
+            shifted = true;
+        }
+
+        if (!shifted)
+            arr[pointer] = (arr[pointer] + 1) % num
+
+        return id;
+    };
+
+})();
