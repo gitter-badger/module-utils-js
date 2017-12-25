f7ee2f16908cb1d52cb3c47f607fafa9c71f9d7f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2017-12-25 14:03:17 +0100

    Fixed ajax headers, added always handler.

--- utils/ajax.js
+++ utils/ajax.js
@@ -4,39 +4,29 @@ exports.ajax = function(obj) {
     var json = 'application/json';
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
+            if (obj.header['Accept'] === json) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (err) {
+                    console.warn('Ajax: Unable to parse server response to JSON.');
+                    obj.error(null, xhr);
+                }
+            }
+            else {
+                res = xhr.responseText;
+            }
             if (xhr.status === 200) {
-                if (obj.header['Accept' === json]) {
-                    try {
-                        res = JSON.parse(xhr.responseText);
-                    } catch (err) {
-                        console.warn('Ajax: Unable to parse success server response to JSON.');
-                        obj.error(null, xhr);
-                    }
-                }
-                else {
-                    res = xhr.responseText;
-                }
                 obj.success(res, xhr);
             } else {
-                if (obj.header['Accept'] === json) {
-                    try {
-                        res = JSON.parse(xhr.responseText);
-                    } catch (err) {
-                        console.warn('Ajax: Unable to parse error server response to JSON.');
-                        obj.error(null, xhr);
-                    }
-                }
-                else {
-                    res = xhr.responseText;
-                }
                 obj.error(res, xhr);
             }
+            obj.always(res, xhr);
         }
     };
     xhr.upload.onprogress = function(state) {
         if (state.lengthComputable) {
             var percentage = (state.loaded / state.total) * 100;
-            obj.progress(percentage.toFixed(2));
+            obj.progress(percentage.toFixed(0));
         }
     };
     xhr.onerror = function() {
@@ -44,7 +34,7 @@ exports.ajax = function(obj) {
     };
     xhr.open(obj.method, url, true);
     for (var k in obj.header) {
-        var v = obj[k];
+        var v = obj.header[k];
         xhr.setRequestHeader(k, v);
     }
     xhr.send(obj.data);
