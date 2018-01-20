37c193b1f77ef8cc58496859b926d757eadd680f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-20 19:24:28 +0100

    At file compiler.js changed tabs indendation to space indendation.

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -2,24 +2,23 @@ var fs = require('fs');
 var path = require('path');
 exports.compileUtils = function(filePaths, accessVariable, moduleName, keys) {
     var start = Date.now();
-	if (!Array.isArray(filePaths) || filePaths.length == 0) {
-		return;
+    if (!Array.isArray(filePaths) || filePaths.length == 0) {
+        return;
     }
     filePaths = filePaths.map(function(str) {
         return path.resolve(str);
     });
     fileStats = getFileStatsWithAtLeastOneKey(filePaths, keys);
     str = '';
-	fileStats.forEach(function(fileStat, fileIndex) {
+    fileStats.forEach(function(fileStat, fileIndex) {
         var lastFile = (fileStats.length === fileIndex + 1);
-		str += compileUtilsFromFile(fileStat.filePath, fileStat.keyCount, accessVariable, moduleName, lastFile, keys);
+        str += compileUtilsFromFile(fileStat.filePath, fileStat.keyCount, accessVariable, moduleName, lastFile, keys);
     });
     var out = fs.readFileSync(path.join(__dirname, 'template.js'), 'utf8');
     out = out.replace(/ACCESS_VAR/g, accessVariable);
     out = out.replace(/^\s*UTILS/m, str);
     fs.writeFileSync('./utils.min.js', out);
     endLog(start, out);
-
 };
 function endLog(start, out) {
     var s = Buffer.byteLength(str, 'utf8');
@@ -44,11 +43,11 @@ function getFileStatsWithAtLeastOneKey(filePaths, keys) {
     return arr;
 }
 function compileUtilsFromFile(filePath, keyCount, accessVariable, moduleName, lastFile, keys) {
-	var temp = '';
+    var temp = '';
     var utils = require(filePath);
     var i = 1;
-	for (var k in utils) {
-		if (!utils.hasOwnProperty(k)) {
+    for (var k in utils) {
+        if (!utils.hasOwnProperty(k)) {
             continue;
         }
         if (Array.isArray(keys) && keys.indexOf(k) < 0) {
@@ -57,37 +56,37 @@ function compileUtilsFromFile(filePath, keyCount, accessVariable, moduleName, la
         var v = utils[k];
         temp += '    ' + k + ': ' + compileModuleValue(k, v, accessVariable, moduleName) + ((i === keyCount && lastFile) ? '' : ',\n');
         i++;
-	}
-	return temp;
+    }
+    return temp;
 }
 function compileModuleValue(key, value, accessVariable, moduleName) {
-	if (!value) {
-		return value;
-	}
-	if (typeof(value) == 'function') {
-		return stringifyScript(value.toString(), accessVariable);
-	}
-	else if (typeof(value) == 'object') {
-		return compileModuleObjectValue(value);
-	}
-	else if (typeof(value) == 'string') {
-		if (key == 'moduleName') {
-			value = moduleName;
-		}
-		return "'" + value + "'";
-	}
-	else if (typeof(value.toString) == 'function') {
-		return '"' + value.toString() + '"';
-	}
-	else {
-		return value;
-	}
+    if (!value) {
+        return value;
+    }
+    if (typeof(value) == 'function') {
+        return stringifyScript(value.toString(), accessVariable);
+    }
+    else if (typeof(value) == 'object') {
+        return compileModuleObjectValue(value);
+    }
+    else if (typeof(value) == 'string') {
+        if (key == 'moduleName') {
+            value = moduleName;
+        }
+        return "'" + value + "'";
+    }
+    else if (typeof(value.toString) == 'function') {
+        return '"' + value.toString() + '"';
+    }
+    else {
+        return value;
+    }
 }
 function compileModuleObjectValue(obj, accessVariable) { // REKURZIA
-	if (obj instanceof Date) {
-		return obj.getTime();
-	}
-	var line = '{';
+    if (obj instanceof Date) {
+        return obj.getTime();
+    }
+    var line = '{';
     var len = Object.keys(obj).length;
     var i = 0;
     for (var k in obj) {
@@ -96,26 +95,26 @@ function compileModuleObjectValue(obj, accessVariable) { // REKURZIA
         v = compileModuleValue(k, v, accessVariable);
         line += k + ':' + v + (i == len ? '' : ',');
     }
-	return line + '}';
+    return line + '}';
 }
 function stringifyScript(input, accessVariable) {
     var last = '';
-	return ('\n' + input + '\n').replace(/(?:(^|[-+\([{}=,:;!%^&*|?~]|\/(?![/*])|return|throw)(?:\s|\/\/[^\n]*\n|\/\*(?:[^*]|\*(?!\/))*\*\/)*(\/(?![/*])(?:\\[^\n]|[^[\n\/\\]|\[(?:\\[^\n]|[^\]])+)+\/)|(^|'(?:\\[\s\S]|[^\n'\\])*'|"(?:\\[\s\S]|[^\n"\\])*"|([0-9A-Za-z_$]+)|([-+]+)|.))(?:\s|\/\/[^\n]*\n|\/\*(?:[^*]|\*(?!\/))*\*\/)*/g, function (str, context, regexp, result, word, operator) {
-		if (word) {
-			if (accessVariable && word == 'exports') {
-				result = accessVariable;
-			}
-			result = (last == 'word' ? ' ' : (last == 'return' ? ' ' : '')) + result;
-			last = (word == 'return' || word == 'throw' || word == 'break' ? 'return' : 'word');
-		} else if (operator) {
-			result = (last == operator.charAt(0) ? '\n' : '') + result;
-			last = operator.charAt(0);
-		} else {
-			if (regexp) {
-				result = context + (context == '/' ? '\n' : '') + regexp;
-			}
-			last = '';
-		}
-		return result;
-	});
+    return ('\n' + input + '\n').replace(/(?:(^|[-+\([{}=,:;!%^&*|?~]|\/(?![/*])|return|throw)(?:\s|\/\/[^\n]*\n|\/\*(?:[^*]|\*(?!\/))*\*\/)*(\/(?![/*])(?:\\[^\n]|[^[\n\/\\]|\[(?:\\[^\n]|[^\]])+)+\/)|(^|'(?:\\[\s\S]|[^\n'\\])*'|"(?:\\[\s\S]|[^\n"\\])*"|([0-9A-Za-z_$]+)|([-+]+)|.))(?:\s|\/\/[^\n]*\n|\/\*(?:[^*]|\*(?!\/))*\*\/)*/g, function (str, context, exp, result, word, operator) {
+        if (word) {
+            if (accessVariable && word == 'exports') {
+                result = accessVariable;
+            }
+            result = (last == 'word' ? ' ' : (last == 'return' ? ' ' : '')) + result;
+            last = (word == 'return' || word == 'throw' || word == 'break' ? 'return' : 'word');
+        } else if (operator) {
+            result = (last == operator.charAt(0) ? '\n' : '') + result;
+            last = operator.charAt(0);
+        } else {
+            if (exp) {
+                result = context + (context == '/' ? '\n' : '') + exp;
+            }
+            last = '';
+        }
+        return result;
+    });
 }
