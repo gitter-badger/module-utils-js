adf1f5210504a6f17e4d3bafbcba0993eaab8243
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-21 20:45:48 +0100

    _internal.js reviewed. Added spaces between parameters in result like in console.log('aaa', 'bbb');, results in 'aaa bbb'.

---
+++ utils/_internal.js
@@ -0,0 +1,43 @@
+exports.__cache = {};
+exports.malloc = function(prefix) {
+    if (!exports.__cache[prefix]) {
+        exports.__cache[prefix] = {};
+    }
+    var obj = exports.__cache[prefix];
+    return function(k, v) {
+        if (typeof(k) !== 'string') {
+            throw new Error('invalidParameter');
+        }
+        if (v === undefined) {
+            return obj[k];
+        }
+        else {
+            obj[k] = v;
+        }
+    }
+};
+exports.toDebugStr = function(args) {
+	var str = '';
+	for (var i = 0; i < arguments.length; i++) {
+		var arg = arguments[i];
+		if (arg) {
+			if (typeof(arg) === 'object' || Array.isArray(arg)) {
+				if (Array.isArray(arg)) {
+					str += 'Array(' + arg.length + '): \n';
+				}
+				else if (typeof(arg) === 'object') {
+					str += 'Object: \n';
+				}
+				str += JSON.stringify(arg, null, '    ');
+				str += '\n';
+			}
+			else {
+				str += (i > 0 ? ' ' : '') + arg;
+			}
+		}
+		else {
+			str += (i > 0 ? ' ' : '') + arg;
+		}
+	}
+	return str;
+};
