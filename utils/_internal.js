d30ca3f88dd73161f824961ce3b3503c9af6ca06
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-21 21:59:58 +0100

    Changed tabs indentation to spaces indentation in _internal.js.

--- utils/_internal.js
+++ utils/_internal.js
@@ -17,27 +17,27 @@ exports.malloc = function(prefix) {
     }
 };
 exports.toDebugStr = function(args) {
-	var str = '';
-	for (var i = 0; i < arguments.length; i++) {
-		var arg = arguments[i];
-		if (arg) {
-			if (typeof(arg) === 'object' || Array.isArray(arg)) {
-				if (Array.isArray(arg)) {
-					str += 'Array(' + arg.length + '): \n';
-				}
-				else if (typeof(arg) === 'object') {
-					str += 'Object: \n';
-				}
-				str += JSON.stringify(arg, null, '    ');
-				str += '\n';
-			}
-			else {
-				str += (i > 0 ? ' ' : '') + arg;
-			}
-		}
-		else {
-			str += (i > 0 ? ' ' : '') + arg;
-		}
-	}
-	return str;
+    var str = '';
+    for (var i = 0; i < arguments.length; i++) {
+        var arg = arguments[i];
+        if (arg) {
+            if (typeof(arg) === 'object' || Array.isArray(arg)) {
+                if (Array.isArray(arg)) {
+                    str += 'Array(' + arg.length + '): \n';
+                }
+                else if (typeof(arg) === 'object') {
+                    str += 'Object: \n';
+                }
+                str += JSON.stringify(arg, null, '    ');
+                str += '\n';
+            }
+            else {
+                str += (i > 0 ? ' ' : '') + arg;
+            }
+        }
+        else {
+            str += (i > 0 ? ' ' : '') + arg;
+        }
+    }
+    return str;
 };
