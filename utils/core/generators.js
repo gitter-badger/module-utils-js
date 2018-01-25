f0a730219ea0090cf76f7100329bd26fcee3bf53
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-25 14:17:42 +0100

    Revision of generators.js 2.

--- utils/core/generators.js
+++ utils/core/generators.js
@@ -24,7 +24,14 @@ exports.UID = function() {
     var day = twoPlaces(d.getDate());
     var h = twoPlaces(d.getHours());
     var min = twoPlaces(d.getMinutes());
-    var arr = [d.getFullYear(), mo, day, h, min, exports.IID(2)];
+    var arr = [
+        d.getFullYear(),
+        mo,
+        day,
+        h,
+        min,
+        exports.SID(3)
+    ];
     return arr.join('-');
     function twoPlaces(v) {
         return v < 10 ? ('0' + v) : v;
@@ -34,47 +41,43 @@ exports.IID = function(len) {
     var c = exports.malloc('__GEN');
     len = parseInt(len);
     len = isNaN(len) ? 3 : len;
+    if (len <= 0) {
+        throw new Error('invalidParameter');
+    }
     var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     var num = chars.length;
     var prefix = 'SID_' + len;
+    var i = 0;
+    var arr = [];
     if (c(prefix)) {
-        var l = c(prefix).len;
-        var arr = c(prefix).arr;
+        arr = c(prefix).arr;
         var b = '';
-        for (i = 0; i < l; i++) {
+        for (i; i < len; i++) {
             b += chars[arr[i]];
         }
-        var end = l - 1;
-        var pointer = c(prefix).pointer;
-        var shifted = false;
-        var last = arr[end];
-        if (last == (num - 1)) {
-            while (arr[pointer] == (num - 1)) {
-                arr[pointer] = 0;
-                pointer--;
+        i = len - 1;
+        if (arr[len - 1] == (num - 1)) { // JUMP
+            while (arr[i] == (num - 1)) {
+                arr[i] = 0;
+                i--;
             }
-            if (pointer != -1) {
-                arr[pointer] = (arr[pointer] + 1) % num;
+            if (i != -1) {
+                arr[i] = (arr[i] + 1) % num;
             }
-            pointer = end;
-            shifted = true;
+            i = len - 1;
         }
-        if (!shifted) {
-            arr[pointer] = (arr[pointer] + 1) % num;
+        else {
+            arr[i] = (arr[i] + 1) % num;
         }
         c(prefix).arr = arr;
-        c(prefix).pointer = pointer;
         return b;
     }
     else {
-        var arr = [];
-        for (var i = 0; i < len; i++) {
+        for (i; i < len; i++) {
             arr[i] = 0;
         }
         c(prefix, {
-            len: len,
-            arr: arr,
-            pointer: len - 1
+            arr: arr
         });
         return exports.IID(len);
     }
