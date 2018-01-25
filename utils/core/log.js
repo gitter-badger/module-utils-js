783d63dce9040c270bf36420b8f2c7517ee09374
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-25 21:48:06 +0100

    Added: logPrefix(). "moduleName" was used for prefixing so we don't need "moduleName" anymore - deleted in previous commit.

--- utils/core/log.js
+++ utils/core/log.js
@@ -1,14 +1,22 @@
+exports.logPrefix = function(str) {
+    var c = exports.malloc('__LOG');
+    c('prefix', str);
+};
 exports.logDebug = function(/*...args*/) {
-	var log = exports.toDebugStr.apply(this, arguments);
-    console.log(exports.moduleName + ': ' + log);
+    var c = exports.malloc('__LOG');
+    var log = exports.toDebugStr.apply(this, arguments);
+    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
+    console.log(pre + log);
 };
 exports.logWarn = function(/*...args*/) {
+    var c = exports.malloc('__LOG');
     var args = [].slice.call(arguments);
-    args.unshift(exports.moduleName + ':');
+    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
     console.warn.apply(null, args);
 };
 exports.log = function(/*...args*/) {
+    var c = exports.malloc('__LOG');
     var args = [].slice.call(arguments);
-    args.unshift(exports.moduleName + ':');
+    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
     console.log.apply(null, args);
 };
