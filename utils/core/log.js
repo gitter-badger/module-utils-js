85902162571fed642f5172f98a09b4d04db08806
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-01-28 18:12:08 +0100

    Updated: "log.js" by eslint rules.

--- utils/core/log.js
+++ utils/core/log.js
@@ -1,22 +1,28 @@
+/* eslint-disable no-console */
 exports.logPrefix = function(str) {
     var c = exports.malloc('__LOG');
     c('prefix', str);
 };
-exports.logDebug = function(/*...args*/) {
-    var c = exports.malloc('__LOG');
-    var log = exports.toDebugStr.apply(this, arguments);
-    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
-    console.log(pre + log);
-};
-exports.logWarn = function(/*...args*/) {
-    var c = exports.malloc('__LOG');
+exports.logDebug = function(/* ...args */) {
+    var cache = exports.malloc('__LOG');
     var args = [].slice.call(arguments);
-    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
+    var log = exports.toDebugStr.apply(this, args);
+    var prefix = cache('prefix') ? (cache('prefix') + ': ') : '';
+    console.log(prefix + log);
+};
+exports.logWarn = function(/* ...args */) {
+    var cache = exports.malloc('__LOG');
+    var args = [].slice.call(arguments);
+    if (cache('prefix')) {
+        args.unshift(cache('prefix') + ':');
+    }
     console.warn.apply(null, args);
 };
-exports.log = function(/*...args*/) {
-    var c = exports.malloc('__LOG');
+exports.log = function(/* ...args */) {
+    var cache = exports.malloc('__LOG');
     var args = [].slice.call(arguments);
-    var pre = c('prefix') && args.unshift(c('prefix') + ': ');
+    if (cache('prefix')) {
+        args.unshift(cache('prefix') + ':');
+    }
     console.log.apply(null, args);
 };
